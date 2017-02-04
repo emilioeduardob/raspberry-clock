7f6327b2439a662501c2a87f7e95c3a749d3c057
Emilio Blanco <emilioeduardob@gmail.com>
2017-02-04 18:34:00 -0300

    refactor(): Created Clock class

--- clock.py
+++ clock.py
@@ -1,67 +1,54 @@
-import pygame, sys, os
-import datetime
+import pygame, os
+from datetime import datetime
 import pytz
 import weather
-try:
-    import buttons
-except:
-    print "Not running in Raspberry"
 
 from pygame.locals import *
 from time import sleep
 os.environ["SDL_FBDEV"] = "/dev/fb1"
 
-font_color = (224, 224, 224)
-TICK_EVENT = USEREVENT + 1
-pygame.time.set_timer(TICK_EVENT, 1000)
-WEATHER_EVENT = TICK_EVENT + 1
-pygame.time.set_timer(WEATHER_EVENT, 15 * 60 * 1000)
+class Clock():
+    FONT_COLOR = (224, 224, 224)
 
-current_weather = weather.Weather('asuncion')
-current_weather.refresh()
-weather_image = current_weather.get_icon_image()
+    def __init__(self, weather):
+        self.weather = weather
+        self.weather_image = weather.get_icon_image()
+        self.init_pygame()
+        self.setup_fonts()
 
-pygame.init()
-pygame.mouse.set_visible(False)
+    def setup_fonts(self):
+        self.clock_font = pygame.font.Font("fonts/digital-mono.ttf", 120)
+        self.weather_font = pygame.font.Font("fonts/digital-mono.ttf", 38)
 
-# set up the window
-DISPLAYSURF = pygame.display.set_mode((320, 240), 0, 32)
-BCK = pygame.image.load("images/night.png")
+    def init_pygame(self):
+        pygame.init()
+        pygame.mouse.set_visible(False)
+        # set up the window
+        self.surface = pygame.display.set_mode((320, 240), 0, 32)
 
-font = pygame.font.Font("fonts/digital-mono.ttf", 120)
-weather_font = pygame.font.Font("fonts/digital-mono.ttf", 38)
+    def set_wallpaper(self):
+        if (self.is_day()):
+            bck = pygame.image.load("images/day.png")
+        else:
+            bck = pygame.image.load("images/night.png")
+        self.surface.blit(bck, (0, 0))
 
-def draw_clock():
-    now = datetime.datetime.now(pytz.timezone('America/Santiago'))
-    if (now.second % 2 == 0):
-        pattern = "{:02d} {:02d}"
-    else:
-        pattern = "{:02d}:{:02d}"
-    label = font.render(pattern.format(now.hour, now.minute), True, font_color)
-    DISPLAYSURF.blit(BCK, (0, 0))
-    DISPLAYSURF.blit(label, (20, 80))
-    if weather_image is not None:
-        DISPLAYSURF.blit(weather_image, (0, 0))
+    def is_day(self):
+        now = datetime.now()
+        return (now > self.weather.sunrise() and now < self.weather.sunset())
 
-    temp = round(current_weather.get_temp())
-    weather_label = weather_font.render("{0:g} C".format(temp), True, font_color)
-    DISPLAYSURF.blit(weather_label, (84, 20))
+    def draw_clock(self):
+        self.set_wallpaper()
+        now = datetime.now(pytz.timezone('America/Santiago'))
+        if (now.second % 2 == 0):
+            pattern = "{:02d} {:02d}"
+        else:
+            pattern = "{:02d}:{:02d}"
+        label = self.clock_font.render(pattern.format(now.hour, now.minute), True, self.FONT_COLOR)
+        self.surface.blit(label, (20, 80))
+        if self.weather_image is not None:
+            self.surface.blit(self.weather_image, (0, 0))
 
-# run the game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == WEATHER_EVENT:
-            current_weather.refresh()
-            weather_image = current_weather.get_icon_image()
-        if event.type == TICK_EVENT:
-            draw_clock()
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print("Pos: %sx%s\n" % pygame.mouse.get_pos())
-            if box.collidepoint(pygame.mouse.get_pos()):
-                pygame.quit()
-                sys.exit()
-    pygame.display.update()
-    sleep(0.1)
+        temp = round(self.weather.get_temp())
+        weather_label = self.weather_font.render("{0:g} C".format(temp), True, self.FONT_COLOR)
+        self.surface.blit(weather_label, (84, 20))
---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame, sys, os
+import datetime
+import pytz
+import weather
+try:
+    import buttons
+except:
+    print "Not running in Raspberry"
+
+from pygame.locals import *
+from time import sleep
+from clock import Clock
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+
+font_color = (224, 224, 224)
+TICK_EVENT = USEREVENT + 1
+pygame.time.set_timer(TICK_EVENT, 1000)
+WEATHER_EVENT = TICK_EVENT + 1
+pygame.time.set_timer(WEATHER_EVENT, 15 * 60 * 1000)
+
+weather = weather.Weather('asuncion')
+weather.refresh()
+clock = Clock(weather)
+
+# run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == WEATHER_EVENT:
+            current_weather.refresh()
+            weather_image = current_weather.get_icon_image()
+        if event.type == TICK_EVENT:
+            clock.draw_clock()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("Pos: %sx%s\n" % pygame.mouse.get_pos())
+            if box.collidepoint(pygame.mouse.get_pos()):
+                pygame.quit()
+                sys.exit()
+    pygame.display.update()
+    sleep(0.1)
